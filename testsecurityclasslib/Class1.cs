90399e3fd5c7c9c2265cf8f30c0db535b53f7cbb
Tom Ordille <thordill@microsoft.com>
2024-04-25 10:17:34 -0400

    Add project files.

---
+++ testsecurityclasslib/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace testsecurityclasslib
+{
+    public class Class1
+    {
+        public string UpdateCustomerPassword(string txtUsername, string txtPassword)
+        {
+            string userName = txtUsername;
+            string password = txtPassword;
+
+            Regex testPassword = new Regex(userName);
+            Match match = testPassword.Match(password);
+            if (match.Success)
+            {
+                return "Do not include name in password.";
+            }
+            else
+            {
+                return "Good password.";
+            }
+        }
+    }
+}
